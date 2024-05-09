86d9bb757eb37c9014672d2f7392c29eadfec490
sjastrow <skjastrow@gmail.com>
2024-05-09 17:33:27 -0400

    created put and delete routes for product.

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -131,6 +131,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(ProductData => {
+    if (!ProductData){
+      res.status(404).json({message: 'No product found with this id'});
+      return;
+    }
+    res.json(ProductData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
