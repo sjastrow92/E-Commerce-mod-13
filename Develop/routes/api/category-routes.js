ad2fb8a976f1b03465d0f60c5943891565091273
sjastrow <skjastrow@gmail.com>
2024-05-09 17:26:10 -0400

    created get route to find one category by its id value.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -25,6 +25,29 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+   Category.findOne({
+    where: {
+      id: req.params.id
+    }, 
+    attributes: [ 'category_name', 'id'],
+    include: [ 
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price']
+      }
+    ]
+  })
+  .then(categoryData => {
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+    }
+    res.json(categoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
